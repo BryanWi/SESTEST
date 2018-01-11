ed994fcd01168cce0ef72f4a3ae739d75bcf1e38
BryanWi <34015543+BryanWi@users.noreply.github.com>
2018-01-10 19:56:31 -0600

    Add files via upload

---
+++ empty-example/sketch.js
@@ -0,0 +1,44 @@
+var ship;
+
+
+var c= 0;
+
+function setup() {
+ 
+
+createCanvas(windowWidth, windowHeight);
+  
+ship= new Ship();
+
+
+}
+
+
+
+
+
+
+function draw() {
+ 
+background(color(0,255-c,c));
+c++;
+
+if(c>=255){c=0;}
+ship.render}
+
+
+
+
+function Ship(){
+  this.r=100;
+  
+
+this.render= function(){
+
+   
+fill(color(0,100,100));
+    triangle(-this.r, this.r, this.r, this.r, 0, -this.r);
+  }
+
+
+}
