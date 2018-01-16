b5f03366db59cce1dd95d7c15d5e69f3cae77c65
BryanWi <34015543+BryanWi@users.noreply.github.com>
2018-01-16 07:48:27 -0600

    Update sketch.js

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,4 +1,34 @@
-var ship;
+var x = 100;
+var y = 100;
+
+function setup() {
+  createCanvas(512, 512);
+}
+
+function draw() {
+  if (keyIsDown(LEFT_ARROW)) {
+    x -= 5;
+  }
+
+  if (keyIsDown(RIGHT_ARROW)) {
+    x += 5;
+  }
+
+  if (keyIsDown(UP_ARROW)) {
+    y -= 5;
+  }
+
+  if (keyIsDown(DOWN_ARROW)) {
+    y += 5;
+  }
+
+  clear();
+  fill(255, 0, 0);
+  ellipse(x, y, 50, 50);
+}
+
+
+/*var ship;
 
 var c= 0;
 var x=0;
@@ -63,4 +93,4 @@ fill(color(0,100,100));
 
 
 }
-*/
+*/*/
